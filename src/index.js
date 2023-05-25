b3a8a6a2a712acbf1f8e6f94e5a881fa85bfce66
Cristian Afonso Nieto <can97clase@gmail.com>
2023-05-25 18:39:20 +0100

    idiomas path

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ i18n
       caches: ['cookie'],
     },
     backend: {
-      loadPath: '/assets/locales/{{lng}}/translation.json',
+      loadPath: './assets/locales/{{lng}}/translation.json',
     }
   });
 
